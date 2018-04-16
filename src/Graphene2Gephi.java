a995a0b6f59f40f4d81c11129c0653b72c9cb0f7
GrayCentipede <maucarrui@gmail.com>
2018-04-15 21:32:44 -0500

    -Specific search for subject/predicates/objects is now available -Moved test files to a new directory

--- src/Graphene2Gephi.java
+++ src/Graphene2Gephi.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.OutputStreamWriter;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.lang.ArrayIndexOutOfBoundsException;
 
 public class Graphene2Gephi {
 
@@ -24,6 +25,20 @@ public class Graphene2Gephi {
           }
      }
 
+     private static void readObjectives(String objFile) throws IOException {
+          try {
+               FileInputStream fileIn = new FileInputStream(objFile);
+               InputStreamReader isIn = new InputStreamReader(fileIn);
+               BufferedReader in = new BufferedReader(isIn);
+
+               p.loadObjectives(in);
+               p.depurate();
+               in.close();
+          } catch (IOException ioe) {
+               throw new IOException("An error occurred while trying to read the file.");
+          }
+     }
+
      private static void save(String gephiFile) throws IOException {
           try {
                FileOutputStream fileOut = new FileOutputStream(gephiFile);
@@ -40,9 +55,22 @@ public class Graphene2Gephi {
      public static void main(String[] args) {
           try{
                read(args[0]);
+               if (args.length > 2) {
+                    if (args[2].equals("-d"))
+                         p.setMode(1);
+                    else if (args[2].equals("-D"))
+                         p.setMode(2);
+                    else{
+                         System.err.println("Invalid flag.");
+                         System.exit(1);
+                    }
+                    readObjectives(args[3]);
+               }
                save(args[1]);
           } catch (IOException ioe) {
                System.out.println(ioe);
+          } catch (ArrayIndexOutOfBoundsException aio) {
+               System.out.println("Usage: \n java -jar graphene2gephi.jar inputFile.nt outputFile.gexf [-d|-D objectiveFile]");
           }
      }
 }
--- src/Parser.java
+++ src/Parser.java
@@ -3,17 +3,28 @@ import java.io.BufferedReader;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.io.IOException;
 
 public class Parser {
-     private ArrayList<String> Nodes;
+     private ArrayList<String[]> Nodes;
      private ArrayList<Object[]> Edges;
+
+     private ArrayList<String> Objectives;
 
      private String file;
 
+     private int mode;
+
      public Parser() {
+          mode = 0;
           Nodes = new ArrayList<>();
           Edges = new ArrayList<>();
+          Objectives = new ArrayList<>();
+     }
+
+     public void setMode(int m) {
+          mode = m;
      }
 
      private String filter(String xml) {
@@ -25,9 +36,23 @@ public class Parser {
           return label;
      }
 
+     public void loadObjectives(BufferedReader in) throws IOException {
+          String line = "";
+          try {
+               do {
+                    line = in.readLine();
+
+                    if (line != null)
+                         Objectives.add(line);
+
+               } while (line != null);
+          } catch (IOException ioe) {
+               throw new IOException("An error ocurred while loading the file");
+          }
+     }
+
      public void load(BufferedReader in) throws IOException {
           String line = "";
-          Boolean verbExists = false;
           int subjectID = 0;
           int objectID = 0;
           String verb = "";
@@ -42,8 +67,15 @@ public class Parser {
                     if (line.indexOf("extraction#subject") > -1 || line.indexOf("extraction#object") > -1) {
 
                          String label = filter(line);
+                         String copy = label.toLowerCase();
 
-                         int z = Nodes.indexOf(label);
+                         int z = -1;
+                         for (String[] s : Nodes) {
+                              if(s[1].toLowerCase().equals(copy)){
+                                   z = Integer.parseInt(s[0]);
+                                   break;
+                              }
+                         }
 
 
                          if (z < 0) {
@@ -51,8 +83,9 @@ public class Parser {
                                    subjectID = Nodes.size();
                               else
                                    objectID = Nodes.size();
-
-                              Nodes.add(label);
+                              String id = Integer.toString(Nodes.size());
+                              String[] node = new String[]{id, label};
+                              Nodes.add(node);
                          }
                          else {
                               if (line.indexOf("extraction#subject") > -1)
@@ -82,6 +115,56 @@ public class Parser {
           }
      }
 
+     public void depurate() {
+          ArrayList<Integer> n = new ArrayList<>();
+          ArrayList<String> keep = new ArrayList<>();
+          Iterator<String[]> j = Nodes.iterator();
+          Iterator<Object[]> i = Edges.iterator();
+
+          for (String[] s : Nodes) {
+               for (String o : Objectives) {
+                    String k = s[1];
+                    if (k.indexOf(o) > -1) {
+                         n.add(Integer.parseInt(s[0]));
+                         break;
+                    }
+               }
+          }
+
+          while (i.hasNext()) {
+               Object[] e = i.next();
+               Boolean found = false;
+               if (!(n.contains(e[1]) || n.contains(e[2]))) {
+                    if (mode == 2) {
+                         for (String o : Objectives) {
+                              found = false;
+                              String aux = (String) e[0];
+                              if (aux.indexOf(o) > -1)
+                                   found = true;
+                         }
+                         if (!found) {
+                              i.remove();
+                              continue;
+                         }
+                    }
+                    else {
+                         i.remove();
+                         continue;
+                    }
+               }
+               String[] t = Nodes.get((int) e[1]);
+               keep.add(t[0]);
+               t = Nodes.get((int) e[2]);
+               keep.add(t[0]);
+          }
+
+          while (j.hasNext()) {
+               String[] s = j.next();
+               if (!(keep.contains(s[0])))
+                    j.remove();
+          }
+     }
+
      @Override public String toString() {
           file = "<gexf version=\"1.3\">\n";
           file += "     <meta lastmodifieddate=\""+ new SimpleDateFormat("yyyy-MM-dd+HH:mm").format(Calendar.getInstance().getTime()) +"\">\n";
@@ -91,8 +174,8 @@ public class Parser {
           file += "          <nodes count=\""+ Nodes.size() +"\">\n";
 
           int x = 0;
-          for (String label : Nodes) {
-               file += "               <node id=\""+ (x++) +"\" label=\""+ label +"\"/>\n";
+          for (String[] node : Nodes) {
+               file += "               <node id=\""+ node[0] +"\" label=\""+ node[1] +"\"/>\n";
           }
 
 
