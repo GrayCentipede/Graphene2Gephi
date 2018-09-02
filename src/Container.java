f6eaebbd222d433b4d4dbeccaef1e39aa0b83f88
GrayCentipede <maucarrui@gmail.com>
2018-09-02 14:22:54 -0500

    Now able of generating html files

---
+++ src/Container.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Collections;
+
+public class Container {
+     HashMap<String, Table> tables;
+
+     public Container() {
+          tables = new HashMap<>();
+     }
+
+     public void load(ArrayList<Parser.Edge> edges) {
+          for (Parser.Edge e : edges) {
+               String relation = e.getRelation();
+               String target = e.getTarget().getLabel();
+               String source = e.getSource().getLabel();
+               Table t = tables.get(source);
+               Table n = null;
+               if (t == null) {
+                    n = new Table(source);
+                    n.addColumn(relation, target);
+                    tables.put(source, n);
+               }
+               else
+                    t.addColumn(relation, target);
+
+               t = tables.get(target);
+               if (t == null) {
+                    n = new Table(target);
+                    n.addColumn(relation, source);
+                    tables.put(target, n);
+               }
+               else
+                    t.addColumn(relation, source);
+          }
+     }
+
+     public void sort() {
+          for (Table t : tables.values())
+               Collections.sort(t.columns, (a,b) -> {
+                    String x = a.relation;
+                    String y = b.relation;
+                    return x.compareTo(y);
+               });
+     }
+
+     @Override public String toString() {
+          String html = "<html>\n";
+          html += "    <head>";
+          html += "        <title> ... </title>\n";
+          html += "        <style>\n";
+          html += "            table, th, td {\n";
+          html += "                border: 1px solid black;\n";
+          html += "                border-collapse: collapse;\n";
+          html += "            }\n";
+          html += "        </style>\n";
+          html += "    </head>\n";
+          html += "    <body>\n";
+
+          for (Table t: tables.values()){
+               html += "<br>";
+               html += t.toString();
+          }
+
+          html += "    </body>\n";
+          html += "</html>\n";
+          return html;
+     }
+}
---
+++ src/Table.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class Table {
+
+     public class Column {
+          String relation;
+          String target;
+
+          public Column(String relation, String target) {
+               this.relation = relation;
+               this.target = target;
+          }
+     }
+
+     public String source;
+     public ArrayList<Column> columns;
+
+     public Table(String source) {
+          if (source.isEmpty())
+               this.source = "' '";
+          else
+               this.source = source;
+          this.columns = new ArrayList<>();
+     }
+
+     public void addColumn(String relation, String target) {
+          Column c = new Column(relation, target);
+          columns.add(c);
+     }
+
+     @Override public String toString() {
+          String tableHTML = "";
+          tableHTML += "    <table align='center' >\n";
+          tableHTML += "        <tr>\n";
+          tableHTML += "            <th colspan='2' align='center'> " + this.source + "</th>\n";
+          tableHTML += "        </tr>\n";
+          tableHTML += "        <tr>\n";
+          tableHTML += "            <td align='center'> Relation </td>\n";
+          tableHTML += "            <td align='center'> Source </td>\n";
+          tableHTML += "        </tr>\n";
+          for (Column c: columns) {
+               tableHTML += "        <tr>\n";
+               tableHTML += "            <td align='center'> "+ c.relation +" </td>\n";
+               tableHTML += "            <td align='center'> "+ c.target +" </td>\n";
+               tableHTML += "        </tr>\n";
+          }
+          tableHTML += "    </table>\n";
+          return tableHTML;
+     }
+}
